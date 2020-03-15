5198d6d64910d85dfbf751db595701e081c61f8b
Ignacio Bejarano <ignbejarano@gmail.com>
2020-03-15 16:16:55 -0300

    improve skills page layout

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,31 +2,17 @@ import React, { Component } from "react"
 import Layout from "../components/Layout/layout"
 
 import "../styles/skills.css"
-import "../styles/content.css"
 
 class WorkPage extends Component {
 
   render() {
-    let workData = this.props.data.workJson
-    const workItems = workData.work_items.map((item, idx) => (
-      <div
-        key={idx}
-        className="work-item"
-      >
-        <div className="work-item-text">
-          <h1>{item.name}</h1>
-          <h2>{item.stack.type}</h2>
-        </div>
-      </div>
-    ))
 
     return (
       <Layout page={"work"}>
-        <div className="content-container">
-          <div className="skills-frontend">
-            <h1>Front End</h1>
-            <p>Me gusta realizar páginas web desde cero, recorriendo el proceso de diseño (Figma) hacia el desarrollo. gusta hacer cosas</p>
-            <ul>
+        <div className="skills-container">
+            <h1 className="frontend-head">Front End</h1>
+            <p className="frontend-text">Me gusta realizar páginas web desde cero, recorriendo el proceso de diseño (Figma) hacia el desarrollo. gusta hacer cosas</p>
+            <ul className="frontend-techs">
               <li>HTML</li> 
               <li>CSS</li> 
               <li>Javascript</li> 
@@ -35,11 +21,11 @@ class WorkPage extends Component {
               <li>Gatsby</li> 
               <li>Material UI</li> 
             </ul>
+          <div className="skill-section-container separator">
           </div>
-          <div className="skills-backend">
-            <h1>Back End</h1>
-            <p>Me gusta hacer cosas</p>
-            <ul>
+            <h1 className="backend-head">Back End</h1>
+            <p className="backend-text">Me gusta hacer cosas</p>
+            <ul className="backend-techs">
               <li>Node.js</li> 
               <li>Express.js</li> 
               <li>MongoDB</li> 
@@ -47,11 +33,11 @@ class WorkPage extends Component {
               <li>GraphQL</li> 
               <li>Prisma</li> 
             </ul>
+          <div className="skill-section-container separator-2">
           </div>
-          <div className="skills-varios">
-            <h1>Otros</h1>
-            <p>Me gusta hacer cosas</p>
-            <ul>
+            <h1 className="varios-head">Otros</h1>
+            <p className="varios-text">Me gusta hacer cosas</p>
+            <ul className="varios-techs">
               <li>git</li> 
               <li>GitHub</li> 
               <li>Bitbucket</li> 
@@ -61,29 +47,10 @@ class WorkPage extends Component {
               <li>Heroku</li> 
               <li>Netlify</li> 
             </ul>
-          </div>
       </div>
       </Layout>
     )
   }
 }
-export default WorkPage
 
-export const query = graphql`
-  query {
-    workJson {
-      id
-      title
-      description
-      text
-      work_items {
-        name
-        description
-        stack {
-          type
-        }
-        techDetails
-      }
-    }
-  }
-`
+export default WorkPage
