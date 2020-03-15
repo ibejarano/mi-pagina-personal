1047d986c11d5cb970fcbdb6d87c000c95dd76a3
Ignacio Bejarano <ignbejarano@gmail.com>
2020-03-15 17:58:18 -0300

    removed small screen

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,45 +1,13 @@
 import React, { Component } from "react"
-import { Link } from "gatsby"
-import menuIcon from "../../images/menu.svg"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 import "./header.css"
 
 class Header extends Component {
-  openSidebar() {
-    document.getElementById("sidebar").style.display = "block"
-  }
-
-  closeSidebar() {
-    document.getElementById("sidebar").style.display = "none"
-  }
   render() {
     let page = this.props.page
     return this.props.smallScreen ? (
-      <div className="sidebar">
-        <button onClick={this.openSidebar} className="sidebar-button-off">
-          <img src={menuIcon} alt="Menu" className="menu-icon"></img>
-        </button>
-        <div
-          class="sidebar-inner"
-          style={{ display: "none" }}
-          id="sidebar"
-          onClick={this.closeSidebar}
-        >
-          <button className="sidebar-button-on">
-            <img src={menuIcon} alt="Menu" className="menu-icon"></img>
-          </button>
-          <Link to="/" className="sidebar-link">
-            <button className="sidebar-item">Inicio</button>
-          </Link>
-          <Link to="/skills" className="sidebar-link">
-            <button className="sidebar-item">Skills</button>
-          </Link>
-          <Link to="/work" className="sidebar-link">
-            <button className="sidebar-item">Proyectos</button>
-          </Link>
-        </div>
-      </div>
+      <div>None</div>
     ) : (
       <header className="header">
         <AniLink fade to="/">
