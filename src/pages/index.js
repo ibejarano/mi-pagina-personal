75e8c775b6514549848a51948f71d6317c4a85bc
Ignacio Bejarano <ignbejarano@gmail.com>
2020-03-16 18:56:10 -0300

    changed some components to pure functions

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import Layout from "../components/Layout/layout"
 
-class IndexPage extends Component {
-
-  render() {
-    const { indexJson } = this.props.data
-
-    return (
-      <Layout page={"home"}>
-        <h1 className="title">{indexJson.title}</h1>
-        <h3 className="subtitle">{indexJson.text}</h3>
-        <img src={indexJson.data[0].image} />
-        <img src={indexJson.data[1].image} />
-      </Layout>
-    )
-  }
+export default function ({data:{indexJson}}) {
+  return (
+    <Layout page={"home"}>
+      <h1 className="title">{indexJson.title}</h1>
+      <h3 className="subtitle">{indexJson.text}</h3>
+      <img src={indexJson.data[0].image} />
+      <img src={indexJson.data[1].image} />
+    </Layout>
+  )
 }
-export default IndexPage
 
 export const query = graphql`
   query {
