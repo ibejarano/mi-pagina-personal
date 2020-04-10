7aa6c4daa65cbe4933110f1a7b0fd0c43ffaa680
Ignacio Bejarano <ignbejarano@gmail.com>
2020-04-09 22:19:49 -0300

    added icons to navbar

--- src/components/Layout/header.js
+++ src/components/Layout/header.js
@@ -1,27 +1,29 @@
 import React, { Component } from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faTwitter,
+  faGithub,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons"
 
 import "./header.css"
 
-function setLinkClass(page, current){
-  return (page === current ? "page-active page-link" : "page-link")
+function setLinkClass(page, current) {
+  return page === current ? "page-active page-link" : "page-link"
 }
 
 export default function Header({ page }) {
   return (
     <header className="header">
-      <AniLink fade to="/" className={setLinkClass(page,'home')}>
+      <AniLink fade to="/" className={setLinkClass(page, "home")}>
         <a>Inicio</a>
       </AniLink>
-      <AniLink fade to="/skills" className={setLinkClass(page,'skills')}>
-        <a>
-          Skills
-        </a>
+      <AniLink fade to="/skills" className={setLinkClass(page, "skills")}>
+        <a>Skills</a>
       </AniLink>
-      <AniLink fade to="/work" className={setLinkClass(page,'work')}>
-        <a>
-          Proyectos
-        </a>
+      <AniLink fade to="/work" className={setLinkClass(page, "work")}>
+        <a>Proyectos</a>
       </AniLink>
       <a
         className="page-link"
@@ -29,6 +31,36 @@ export default function Header({ page }) {
         target="_blank"
       >
         Blog
+      </a>
+      <a
+        className="page-link"
+        href="#"
+        target="_blank"
+        style={{
+          padding: "1em",
+        }}
+      >
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a
+        className="page-link"
+        href="#"
+        target="_blank"
+        style={{
+          padding: "1em",
+        }}
+      >
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <a
+        className="page-link"
+        href="#"
+        target="_blank"
+        style={{
+          padding: "1em",
+        }}
+      >
+        <FontAwesomeIcon icon={faInstagram} />
       </a>
     </header>
   )
